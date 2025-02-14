bddb2967b58a8e8db49eaadc98a90fbe2ca75e77
AbelSantiagoG <abelsantiago1000@gmail.com>
2025-02-14 17:31:07 -0500

    NetInfo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,7 @@ const HomeLayout = () => {
       <Stack.Screen name = "index" options= {{headerShown: false}}/>
       <Stack.Screen name = "dashboard" options= {{headerShown: false}}/>
       <Stack.Screen name = "login" options= {{headerShown: false}}/>
-      <Stack.Screen name = "register" options= {{headerShown: false}}/>
+      <Stack.Screen name = "register" options= {{headerShown: isConnected}}/>
     </Stack>
   )
 }
