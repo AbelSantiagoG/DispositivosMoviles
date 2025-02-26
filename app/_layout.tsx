b95f0add424c08de8f50feba3cfdf03140e4699a
MartinOstios <martin.ostios.arias@gmail.com>
2025-02-25 21:12:02 -0500

    Vista cuando no se tiene internet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
 import { useEffect, useState } from "react";
 import NetInfo from '@react-native-community/netinfo'
 
@@ -11,18 +11,27 @@ const HomeLayout = () => {
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
       setIsConnected(state.isConnected as boolean)
+      if (!state.isConnected) {
+        router.replace('/no-connection')
+      }
     })
     return () => {
       unsubscribe();
     }
   }, [])
 
+  if (!isConnected) {
+    return null;
+  }
+
   return (
+    
     <Stack>
       <Stack.Screen name = "index" options= {{headerShown: false}}/>
       <Stack.Screen name = "dashboard" options= {{headerShown: false}}/>
       <Stack.Screen name = "login" options= {{headerShown: false}}/>
       <Stack.Screen name = "register" options= {{headerShown: false}}/>
+      <Stack.Screen name = "no-connection" options= {{headerShown: false}}/>
     </Stack>
   )
 }
---
+++ app/no-connection.tsx
@@ -0,0 +1,25 @@
+import { View, Text, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function NoConnection() {
+    return (
+        <SafeAreaView className="flex-1 bg-black">
+            <View className="items-center justify-center mt-10">
+                <View className="w-52 h-20 mb-4">
+                    <Image className="w-full h-full" source={require("../assets/logo.png")} resizeMode="contain" />
+                </View>
+            </View>
+            
+            <View className="flex-1 items-center justify-center px-4">
+                <MaterialCommunityIcons name="wifi-off" size={100} color="white" />
+                <Text className="text-white text-2xl font-bold text-center mt-6">
+                    Sin conexión a Internet
+                </Text>
+                <Text className="text-gray-400 text-center mt-4 text-lg">
+                    Por favor, verifica tu conexión a internet e intenta nuevamente
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
+} 
